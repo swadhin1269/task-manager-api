3ef163c1110ddf566e70d1c390136b8b198b8831
swadhin1269 <profile.swadhin@gmail.com>
2026-09-05 12:10:26 +0530

    server port url

--- src/server.js
+++ src/server.js
@@ -18,5 +18,5 @@ app.get("/health", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port: http://localhost:${PORT}/`);
 });
